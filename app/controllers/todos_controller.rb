6daa8ec47461c8b669f62a5f9c48002c5ec20a85
nikhils777 <nsharmatennis@gmail.com>
2014-06-14 15:28:24 -0400

    Added basic TDD features

---
+++ app/controllers/todos_controller.rb
@@ -0,0 +1,20 @@
+class TodosController < ApplicationController
+  def index
+  end
+  def new
+  end
+  def show
+    @todo = Todo.find params[:id]
+  end
+  def create
+    @todo = Todo.new(todo_params)
+    @todo.save
+    redirect_to @todo, notice: 'Your new TODO was saved'
+  end
+
+  private
+
+  def todo_params
+    params.require(:todo).permit(:description)
+  end
+end
