0374dff7b8a0964a5a630480304d473a366adeff
nikhils777 <nsharmatennis@gmail.com>
2014-06-15 15:21:05 -0400

    finished basic user functionality

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -11,7 +11,7 @@ class TodosController < ApplicationController
   def create
     @todo = current_user.todos.build(todo_params)
     @todo.save
-    redirect_to @todo, notice: 'Your new TODO was saved'
+    redirect_to root_path , notice: 'Your new TODO was saved'
   end
   def destroy
     @todo = Todo.find(params[:id])
